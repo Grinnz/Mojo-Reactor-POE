1ab9e16422150c667494924a78033965d4bf9fbe
Dan Book <grinnz@grinnz.com>
2015-01-02 14:04:58 -0500

    Bump version

--- lib/Mojo/Reactor/POE.pm
+++ lib/Mojo/Reactor/POE.pm
@@ -8,7 +8,7 @@ use Scalar::Util 'weaken';
 use constant { POE_IO_READ => 0, POE_IO_WRITE => 1 };
 use constant DEBUG => $ENV{MOJO_REACTOR_POE_DEBUG} || 0;
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 my $POE;
 
