e5fe331f24f2b29d57c69154f8d4999ad975f247
Dan Book <grinnz@grinnz.com>
2020-04-19 21:10:59 -0400

    bump to version 1.000

--- lib/Mojo/Reactor/POE.pm
+++ lib/Mojo/Reactor/POE.pm
@@ -14,7 +14,7 @@ use Scalar::Util 'weaken';
 use constant { POE_IO_READ => 0, POE_IO_WRITE => 1 };
 use constant DEBUG => $ENV{MOJO_REACTOR_POE_DEBUG} || 0;
 
-our $VERSION = '0.010';
+our $VERSION = '1.000';
 
 my $POE;
 
